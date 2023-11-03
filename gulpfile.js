c932f7cbd82387a3d628ae1500afb5b4f72e1812
Colin Conn <81298398+colinconn@users.noreply.github.com>
2023-11-03 07:31:01 -0500

    Source Logical Theme Change

--- gulpfile.js
+++ gulpfile.js
@@ -15,12 +15,11 @@ const fs = require('fs');
 
 // postcss plugins
 const autoprefixer = require('autoprefixer');
-const colorFunction = require('postcss-color-mod-function');
 const cssnano = require('cssnano');
 const easyimport = require('postcss-easy-import');
 
-const REPO = 'TryGhost/Casper';
-const REPO_READONLY = 'TryGhost/Casper';
+const REPO = 'TryGhost/Source';
+const REPO_READONLY = 'TryGhost/Source';
 const CHANGELOG_PATH = path.join(process.cwd(), '.', 'changelog.md');
 
 function serve(done) {
@@ -46,10 +45,9 @@ function hbs(done) {
 
 function css(done) {
     pump([
-        src('assets/css/*.css', {sourcemaps: true}),
+        src('assets/css/screen.css', {sourcemaps: true}),
         postcss([
             easyimport,
-            colorFunction(),
             autoprefixer(),
             cssnano()
         ]),
@@ -65,7 +63,7 @@ function js(done) {
             'assets/js/lib/*.js',
             'assets/js/*.js'
         ], {sourcemaps: true}),
-        concat('casper.js'),
+        concat('source.js'),
         uglify(),
         dest('assets/built/', {sourcemaps: '.'}),
         livereload()
@@ -130,7 +128,7 @@ exports.release = async () => {
         const compatibleWithGhost = result.compatibleWithGhost;
 
         const releasesResponse = await releaseUtils.releases.get({
-            userAgent: 'Casper',
+            userAgent: 'Source',
             uri: `https://api.github.com/repos/${REPO_READONLY}/releases`
         });
 
@@ -160,7 +158,7 @@ exports.release = async () => {
             preRelease: false,
             tagName: 'v' + newVersion,
             releaseName: newVersion,
-            userAgent: 'Casper',
+            userAgent: 'Source',
             uri: `https://api.github.com/repos/${REPO}/releases`,
             github: {
                 token: githubToken
